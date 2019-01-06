db46ac5369319875a324dbc5ce755a73888e4bfe
vbakic <vbakicmeister@gmail.com>
2019-01-06 11:41:49 +0100

    further improvements

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -11,7 +11,6 @@ contract Remittance is Pausable, Transferrable {
     uint public claimBackPeriod;
     
     struct Deposit {
-        address remitter;
         uint balance;
         uint revertUntil;
         uint claimBackAfter;
@@ -43,43 +42,47 @@ contract Remittance is Pausable, Transferrable {
         return false;
     }
 
-    function calculateHash(bytes32 password, address remitter) public pure returns(bytes32 hash) {
+    function calculateHash(bytes32 plainPassword, address remitter) public view returns(bytes32 hashedPassword) {
         require(remitter != address(0), "Error: invalid address");
-        return keccak256(abi.encodePacked(password, remitter));
+        return keccak256(abi.encodePacked(plainPassword, remitter, address(this)));
     }
     
-    function depositEther(bytes32 hash, address remitter) public payable onlyIfRunning onlyIfAlive returns (bool success) {
-        uint balance = deposits[hash].balance;
+    function depositEther(bytes32 hashedPassword, address remitter) public payable onlyIfRunning onlyIfAlive returns (bool success) {
+        Deposit memory deposit = deposits[hashedPassword];
+        uint balance = deposit.balance;
         require(msg.value != 0, "Error: no ether provided");
         require(remitter != msg.sender, "Error: deposit to own account not permited");
-        require(deposits[hash].remitter != remitter, "Error: new password required");
+        //the line below should prevent using previous password for the same remitter, it would require that the storage block hasn't been used before
+        require(deposit.revertUntil == 0 && deposit.originalSender == address(0), "Error: new password required");
         emit LogDepositEther(msg.sender, remitter, msg.value);
-        deposits[hash].balance = balance.add(msg.value);
-        deposits[hash].remitter = remitter;
-        deposits[hash].revertUntil = block.number.add(revertPeriod);
-        deposits[hash].claimBackAfter = block.number.add(claimBackPeriod);
-        deposits[hash].originalSender = msg.sender;
+        deposit.balance = balance.add(msg.value);
+        deposit.revertUntil = block.number.add(revertPeriod);
+        deposit.claimBackAfter = block.number.add(claimBackPeriod);
+        deposit.originalSender = msg.sender;
+        deposits[hashedPassword] = deposit;
         return true;
     }
 
-    function claimBackEther(bytes32 hash) public onlyIfRunning onlyIfAlive returns (bool success) {
-        require(msg.sender == deposits[hash].originalSender, "Error: only original sender can claim back funds");
-        uint balance = deposits[hash].balance;
+    function claimBackEther(bytes32 hashedPassword) public onlyIfRunning onlyIfAlive returns (bool success) {
+        Deposit memory deposit = deposits[hashedPassword];
+        require(msg.sender == deposit.originalSender, "Error: only original sender can claim back funds");
+        uint balance = deposit.balance;
         require(balance != 0, "Error: insufficient funds");
-        require(isEligibleForClaimBack(deposits[hash].revertUntil, deposits[hash].claimBackAfter), "Error: not eligible for claim back");
+        require(isEligibleForClaimBack(deposit.revertUntil, deposit.claimBackAfter), "Error: not eligible for claim back");
         emit LogClaimBackEther(msg.sender, balance);
-        deposits[hash].balance = 0;
+        deposit.balance = 0;
+        deposits[hashedPassword] = deposit;
         msg.sender.transfer(balance);
         return true;
     }
     
-    function withdrawEther(bytes32 password) public returns (bool success) {
-        bytes32 hash = calculateHash(password, msg.sender); //only remitter could create the hash, without passing its address
-        require(msg.sender == deposits[hash].remitter, "Error: requested funds are not yours");
-        uint balance = deposits[hash].balance;
+    function withdrawEther(bytes32 plainPassword) public returns (bool success) {
+        //only remitter could create the hashedPassword, without passing its address
+        bytes32 hashedPassword = calculateHash(plainPassword, msg.sender);
+        uint balance = deposits[hashedPassword].balance;
         require(balance != 0, "Error: no ether available or already withdrawn");
         emit LogWithdrawEther(msg.sender, balance);
-        deposits[hash].balance = 0;
+        deposits[hashedPassword].balance = 0;
         msg.sender.transfer(balance);
         return true;
     }
